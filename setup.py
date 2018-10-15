b8bb1d066f8ccebc8dadf266c4ba48efaacd261f
Wallace Breza <wbreza@siriusiqmobile.com>
2018-10-15 16:17:27 -0700

    Updated readme and setup

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='cloud_scanner',
-      version='0.1',
+      version='0.0.1',
       description='Core package for scanning cloud resources across providers',
-      url='',
-      author='Tanner Barlow',
-      author_email='tanner.barlow12@gmail.com',
+      url='https://github.com/Microsoft/cloud-scanner',
+      author='Microsoft',
+      author_email='tanner.barlow12@microsoft.com,wallace.breza@microsoft.com',
       license='MIT',
       packages=find_packages(),
       install_requires=[
