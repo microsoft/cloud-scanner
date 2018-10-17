d8be79a6f860f6b05cbeca45a27c8088186c6fd3
Travis CI <tanner.barlow12@gmail.com>
2018-10-17 08:50:06 -0700

    Add travis and build script

---
+++ build.sh
@@ -0,0 +1,32 @@
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+remove_unnecessary_files(){
+  find . -maxdepth 1 -type f -not \( -name '*.html' -or -name '*.js' -or -name '*.css' \) -delete
+  find . -maxdepth 1 -type d -not \( -name ".git" -or -name "." -or -name "generated" -or -name "_static" \) -exec rm -irf {} \; 
+}
+
+commit_website_files() {
+  git checkout -b gh-pages
+  mv docs/build/html new-docs
+  rm -rf docs
+  mv new-docs docs
+  cp -r docs/* .
+  remove_unnecessary_files
+  touch .nojekyll
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/Microsoft/cloud-scanner.git > /dev/null 2>&1
+  git push origin-pages gh-pages --force
+}
+
+pytest
+make sphinx
+setup_git
+commit_website_files
+upload_files
