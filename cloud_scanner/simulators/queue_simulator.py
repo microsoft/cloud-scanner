80512ba18a64b557ae7b8be8f1d5133f31a3a516
Travis CI <tanner.barlow12@gmail.com>
2018-10-19 07:50:05 -0700

    lint fix for simulators

--- cloud_scanner/simulators/queue_simulator.py
+++ cloud_scanner/simulators/queue_simulator.py
@@ -1,14 +1,12 @@
 import collections
 
-from cloud_scanner.contracts.queue import Queue
-from cloud_scanner.contracts.queue_factory import register_queue_service
+from cloud_scanner.contracts import Queue, register_queue_service
 
 
-@register_queue_service("simulator", lambda queue_name: QueueSimulator(queue_name))
+@register_queue_service("simulator",
+                        lambda queue_name: QueueSimulator(queue_name))
 class QueueSimulator(Queue):
-    """
-    Simulator of queue service
-    """
+    """Simulator of queue service."""
 
     def __init__(self, queue_name, config=None):
         self._queue_name = queue_name
@@ -18,23 +16,23 @@ class QueueSimulator(Queue):
         return len(self._queue)
 
     def push(self, message):
-        """
-        Push new message to queue
+        """Push new message to queue.
+
         :param message: message to push
         :return: None
         """
         self._queue.append(message)
 
     def pop(self):
-        """
-        Pop message off of queue
+        """Pop message off of queue.
+
         :return: Message popped
         """
         return self._queue.popleft()
 
     def peek(self):
-        """
-        Peek at message, but don't pop
+        """Peek at message, but don't pop.
+
         :return: Message peeked
         """
         return self._queue[0]
