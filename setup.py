d8070a412fb2c61568aad0bec248f7aa8c674469
Tanner Barlow <tanner.barlow12@gmail.com>
2018-10-25 08:20:46 -0700

    Fix email of setup.py (#26)
    * fix email of setup.py
    
    * Updated email address

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 exec(open('cloud_scanner/version.py').read())
-setup(name='cloud_scanner_core',
+setup(name='cloud_scanner',
       version=__version__,
       description='Core package for scanning cloud resources across providers',
       url='https://microsoft.github.io/cloud-scanner',
       author='Microsoft',
-      author_email='tanner.barlow@microsoft.com,wallace.breza@microsoft.com',
+      author_email='cloudscanner@microsoft.com',
       license='MIT',
       packages=find_packages(),
       install_requires=[
